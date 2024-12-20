6ad02c5bf038df48a3a81272525150f4a2609df9
orzaca <140673379+orzaca@users.noreply.github.com>
2024-12-20 15:54:28 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# Zirius.v2
-Nueva version
+"# Sirius" 
---
+++ db.php
@@ -0,0 +1,20 @@
+<?php
+// Archivo: db.php
+$host = 'sql308.infinityfree.com';
+$db   = 'if0_37765144_zirius';
+$user = 'if0_37765144';
+$pass = 'atlanticQI1980*';
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+
+try {
+    $pdo = new PDO($dsn, $user, $pass, $options);
+} catch (\PDOException $e) {
+    throw new \PDOException($e->getMessage(), (int)$e->getCode());
+}
